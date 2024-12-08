71e027f22e47cf2606fe9ba941f9daef03964152
gregorymagregory <justfreezenyt@gmail.com>
2024-12-08 13:26:28 +0300

    Озвучка для TossCoin и Apologises. Теперь TossCoin работает корректно

--- src/services (Team 2)/Apologies.py
+++ src/services (Team 2)/Apologies.py
@@ -1,12 +1,16 @@
 import random
 
+from SpeechUtils import speak
+
 
 class Apologies:
     def playApologies(self):
         config = ["Извините!", "Простите",
                   "Пардон", "Виноват", "Это моя вина!", "Прости, я пыталась", "меня подменили роботы",
                   "Извините, я не хотела вас обидеть. Хотя, возможно, я этого и хотела, но потом передумала"]
-        print(random.choice(config))
+        excuses = random.choice(config)
+        print(excuses)
+        speak(excuses)
 
 
 if __name__ == "__main__":
--- src/services (Team 2)/TossCoin.py
+++ src/services (Team 2)/TossCoin.py
@@ -1,10 +1,20 @@
 import random
 
+from SpeechUtils import speak
+
+
 class TossCoin:
 
-    def tossCoin(self):
+    def tossCoin():
         config = ["орел","решка"]
-        print(random.choice(config))
+        TossCoinResult = random.choice(config)
+        if TossCoinResult == "решка":
+            print(f"Вам выпала {TossCoinResult}")
+            speak(f"Вам выпала {TossCoinResult}")
+        if TossCoinResult == "орёл":
+            print(f"Вам выпал {TossCoinResult}")
+            speak(f"Вам выпал {TossCoinResult}")
+
 
 if __name__ == "__main__":
     toss = TossCoin()
